79ccadc8629048beeebd3f7978c620b8c50805e3
Thiago <thiagoaugusto_18@yahoo.com.br>
2021-09-14 17:43:34 -0300

    terceiro

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var hamburguer = document.querySelector(".hamburguer");
+var hamburguer1 = document.querySelector(".hamburguer");
+
 
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
